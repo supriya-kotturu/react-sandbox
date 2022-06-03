8b27566f9d631f83285587930e14695bc7a74322
supriya-kotturu <supriyakotturu@gmail.com>
2022-06-03 22:34:49 +0530

    add useEffectHook

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,20 @@ function App() {
 	const [count, setCount] = useState(0);
 
 	useEffect(() => {
-		const timer = setInterval(() => {
-			console.log('Timer is ticking every 30000 milliseconds');
-		}, 30000);
+		const timer = setTimeout(() => {
+			console.log(
+				'Timeout is run after 1000 milliseconds. But will be cleared by the return function',
+			);
+			console.warn(
+				'This will run only one time, since the dependancy array is empty.',
+			);
+		}, 1000);
 
 		return () => {
-			clearInterval(timer);
+			console.log(
+				'this will run when this component unmounts. Since this is the main component, you might never see this log',
+			);
+			clearTimeout(timer);
 		};
 	}, []);
 
