b55f8f864f2ddb92ac31a9ee3cedf55291abb16b
supriya-kotturu <supriyakotturu@gmail.com>
2022-06-10 23:51:06 +0530

    add useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+	const userNameRef = useRef<HTMLInputElement>(null);
+	const [userName, setUserName] = useState<String>('');
+
 	useEffect(() => {
 		const timer = setTimeout(() => {
 			console.log('timer in typescript');
@@ -11,7 +14,21 @@ function App() {
 			clearTimeout(timer);
 		};
 	}, []);
-	return <div className='App'>Check console to view the timer</div>;
+
+	const handleClick = () => {
+		console.log(userNameRef?.current?.value);
+		setUserName(userNameRef?.current?.value || '');
+	};
+
+	return (
+		<div className='App'>
+			<input type='text' ref={userNameRef} /> <br />
+			<button onClick={handleClick}>getName</button>
+			Check console to view the timer
+			<br />
+			user : {userName}
+		</div>
+	);
 }
 
 export default App;
